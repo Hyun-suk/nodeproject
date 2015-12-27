d7299582db842627f2079df2c8db0432f6da4cd2
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2015-12-27 16:48:50 +0000

    Route add

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ app.get('/', function (req, res) {
 app.set("view engine", 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(__dirname);
+
+var data={count:0};
+
 
 app.get('/', function (req, res) {
-    res.render('my_ejs');
+    data.count++;
+    res.render('my_ejs', data);
 });
+
+
+app.get('/reset', function (req, res) {
+    data.count=0;
+    res.render('my_ejs', data);
+});
+
+
+app.get('set/count', function (req, res) {
+    if(req.query.count) data.count = req.query.count;
+    res.render('my_ejs', data);
+});
+
+
+app.get('/set/:num', function (req, res) {
+    data.count=req.params.num;
+    res.render('my_ejs', data);
+});
+
 
 app.listen(8000, '0.0.0.0', function(){
     console.log('Server On!');
