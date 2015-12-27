828821abd1a772d61b5fc5b418c2308b660678cb
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2015-12-27 15:47:37 +0000

    Static file Add

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var express = require('express');
 
 var app = express();
 
+
+/*
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+*/
 
-ap.listen(3000, function(){
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
+app.listen(8000, '0.0.0.0', function(){
     console.log('Server On!');
 });
