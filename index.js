82281a29cbce6ab63f27456ff3b0c4f1963c4226
EC2 Default User <ec2-user@ip-172-31-22-206.ap-northeast-1.compute.internal>
2015-12-27 16:22:42 +0000

    dynamic web add

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 */
-
+app.set("view engine", 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(__dirname);
+
+app.get('/', function (req, res) {
+    res.render('my_ejs');
+});
+
 app.listen(8000, '0.0.0.0', function(){
     console.log('Server On!');
 });
